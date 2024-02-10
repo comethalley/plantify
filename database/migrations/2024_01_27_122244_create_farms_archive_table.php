5dbbe6eef5c2dd96e73f6901b53dd30e9532be30
ITI-001 <jamespimo23@gmail.com>
2024-02-10 16:12:31 +0800

    feb10,2024farmmanagement

--- database/migrations/2024_01_27_122244_create_farms_archive_table.php
+++ database/migrations/2024_01_27_122244_create_farms_archive_table.php
@@ -1,11 +1,17 @@
+
 <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFarmsArchiveTable extends Migration
+return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up()
     {
         Schema::create('archivefarms', function (Blueprint $table) {
@@ -36,4 +42,4 @@ class CreateFarmsArchiveTable extends Migration
     {
         Schema::dropIfExists('archivefarms');
     }
-}
+};

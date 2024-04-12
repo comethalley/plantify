d8bcd73e94bd72c333cb9d85cb79749b268bacb9
Edman Cabote <edman.cabote99@gmail.com>
2024-04-12 20:14:09 +0800

    landing page change color

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -22,20 +22,15 @@
     <!-- custom Css-->
     <link href="assets/css/custom.min.css" rel="stylesheet" type="text/css" />
 
+   
 </head>
 
-<body>
+<body style="background-color: #8BE262;">
 
     <div class="auth-page-wrapper pt-5">
         <!-- auth page bg -->
-        <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
-            <div class="bg-overlay"></div>
-
-            <div class="shape">
-                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120">
-                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
-                </svg>
-            </div>
+        <div class="auth-one-bg-position " id="auth-particles">
+            
         </div>
 
         <!-- auth page content -->

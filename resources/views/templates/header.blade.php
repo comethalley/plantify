984b626d0851b1ce93ae08c1651f6d191bddc182
Edman Cabote <edman.cabote99@gmail.com>
2024-04-23 20:54:14 +0800

    mga inayos par

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -564,19 +564,25 @@
                 </br>
                 <a href="/dashboard/analytics" class="logo logo-dark">
                     <span class="logo-sm">
-                        <img src="{{ asset('assets/images/p-white-small.png') }}" alt="" height="22" />
+                        <img src="{{ asset('assets/images/bg/pwhitesmall.png') }}" alt="" height="22" />
                     </span>
                     <span class="logo-lg">
-                        <img src="{{ asset('assets/images/p-white.png') }}" alt="" height="17" />
+                    <div style="display: flex; justify-content: center;">
+                        <img src="{{ asset('assets/images/bg/dash.png') }}" alt="" height="50" />
+                    </div>
                     </span>
                 </a>
                 <!-- Light Logo-->
                 <a href="/dashboard/analytics" class="logo logo-light">
                     <span class="logo-sm">
-                        <img src="{{ asset('assets/images/p-white-small.png') }}" alt="" height="22" />
+                        <img src="{{ asset('assets/images/bg/pwhitesmall.png') }}" alt="" height="22" />
                     </span>
+
                     <span class="logo-lg">
-                        <img src="{{ asset('assets/images/p-white.png') }}" alt="" height="40" />
+                    <div style="display: flex; justify-content: center;">
+                        <img src="{{ asset('assets/images/bg/dash.png') }}" alt="" height="50" />
+                    </div>
+
                     </span>
                 </a>
                 <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover" id="vertical-hover">
@@ -588,8 +594,8 @@
                 <div class="container-fluid">
                     <div id="two-column-menu"></div>
                     <ul class="navbar-nav" id="navbar-nav">
-                        <li class="menu-title"><span data-key="t-menu" style="color:white;"></span></li>
-                        </br>
+                        <!-- <li class="menu-title"><span data-key="t-menu" style="color:white;"></span></li> -->
+                        <br>
                         <li class="nav-item">
                             <a class="nav-link menu-link" href="/dashboard/analytics" role="button" style="color:white">
                                 <i class="ri-dashboard-2-line"></i>

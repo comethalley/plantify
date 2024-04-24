9593c57e51cc0cfa4c2c91274a5ed2c54a45cf0b
reign <reignvalo12@gmail.com>
2024-04-25 01:03:17 +0800

    aw

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -482,7 +482,7 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/');
     }
 
 

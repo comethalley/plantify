d8306df37fa95e1630abcc8f82f48816dda92583
jaysonkarl <143271874+jaysonkarl@users.noreply.github.com>
2024-04-30 12:34:15 +0800

    Weather-User-Fix-UI

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -179,9 +179,18 @@ $(document).ready(function () {
                         errorMessage = "Validation error occurred.";
                     }
 
-                    alert(errorMessage);
+                    Swal.fire({
+                        title: "Validation Error",
+                        text: errorMessage,
+                        icon: "error",
+                    });
                 } else {
                     console.error("Error:", error);
+                    Swal.fire({
+                        title: "Error",
+                        text: "An error occurred while processing your request. Please try again later.",
+                        icon: "error",
+                    });
                 }
             },
         });

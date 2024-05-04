551a8043bcea2d81fa94623610230689c6af347f
defnotdrei <andreicompetente027@gmail.com>
2024-05-05 07:37:03 +0800

    sige

--- resources/views/pages/chat.blade.php
+++ resources/views/pages/chat.blade.php
@@ -125,20 +125,20 @@
         
                                         <div class="chat-message-list">
                                             <ul class="list-unstyled chat-list chat-user-list mb-0" id="channelList">
-                                                @forelse($groups as $group)
-                                                    @if(auth()->user()->role_id == 2 && $group->group_name == 'Admin and Farm Leaders')
-                                                        {{-- Display only for role_id 2 (Admin and Farm Leaders) --}}
-                                                        <button type="button" class="btn channel-button" data-group-id="{{ $group->id }}" data-farm-id="{{ optional($farmLeaders)->id }}">
-                                                            <div class="d-flex align-items-center">
-                                                            <img class="rounded-circle header-profile-user" src="{{asset('assets/images/plantifeedpics/rounded.png')}}" alt="Header Avatar">
-                                                                <div class="ms-2">
-                                                                    <h6 class="mb-0">{{ $group->group_name }}</h6>
-                                                                    @if ($group->unread_message_count > 0)
-                                                                        <span class="position-absolute topbar-badge fs-10 translate-end badge rounded-pill bg-danger">{{ $group->unread_message_count }}</span>
-                                                                    @endif
-                                                                </div>
+                                            @forelse($groups as $group)
+                                                @if(in_array(auth()->user()->role_id, [1, 2]) && $group->group_name == 'Admin and Farm Leaders')
+                                                    {{-- Display only for role_id 1 or 2 (Admin and Farm Leaders) --}}
+                                                    <button type="button" class="btn channel-button" data-group-id="{{ $group->id }}" data-farm-id="{{ optional($farmLeaders)->id }}">
+                                                        <div class="d-flex align-items-center">
+                                                            <img class="rounded-circle header-profile-user" src="{{ asset('assets/images/plantifeedpics/rounded.png') }}" alt="Header Avatar">
+                                                            <div class="ms-2">
+                                                                <h6 class="mb-0">{{ $group->group_name }}</h6>
+                                                                @if ($group->unread_message_count > 0)
+                                                                    <span class="position-absolute topbar-badge fs-10 translate-end badge rounded-pill bg-danger">{{ $group->unread_message_count }}</span>
+                                                                @endif
                                                             </div>
-                                                        </button>
+                                                        </div>
+                                                    </button>
                                                     @elseif(auth()->user()->role_id == 3)
                                                         {{-- Display for role_id 3 (both Admin and Farm Leaders, Farm Leader and Farmers) --}}
                                                         <button type="button" class="btn channel-button" data-group-id="{{ $group->id }}" data-farm-id="{{ optional($farmLeaders)->id }}">

7c2095710b7e3a65d2e69c5ab106043114869dd5
jaycol19 <147596653+jaycol19@users.noreply.github.com>
2024-05-16 01:12:49 +0800

    edit

--- resources/views/pages/form.blade.php
+++ resources/views/pages/form.blade.php
@@ -158,7 +158,7 @@
 
                 <div class="card-body">
                     <div class="card-title-container justify-text-center">
-                        <h1 class="card-title text-center text-justify custom-title" style="font-size: 1.3rem; margin-bottom: 15px;"><strong>Pre-Registration Form</strong></h1>
+                        <h1 class="card-title text-center text-justify custom-title" style="font-size: 1.3rem; margin-bottom: 15px;"><strong>Attendees Form</strong></h1>
                         <h5 class="card-title text-center mb-2"><strong>Event Name: {{ $event->title }}</strong></h5>
                         <p class="card-text text-center mb-2"><strong>Date: {{ date('F j, Y', strtotime($event->start)) }} to {{ date('F j, Y', strtotime($event->end)) }}</strong></p>
                         <p class="card-text text-center" style="border-bottom: 1px solid #000;"><strong>{{ date('g:i A', strtotime($event->starttime)) }} to {{ date('g:i A', strtotime($event->endtime)) }}</strong></p>
@@ -185,7 +185,7 @@
                                             <div class="col-md-2">
                                                 <div>
                                                     <label for="middleInitial" class="form-label">M.I</label>
-                                                    <input type="text" class="form-control" id="middleInitial" name="middle_initial" placeholder="M.I" maxlength="3" required>
+                                                    <input type="text" class="form-control" id="middleInitial" name="middle_initial" placeholder="M.I" maxlength="3" >
                                                 </div>
                                             </div>
                                         </div><!-- end row -->
